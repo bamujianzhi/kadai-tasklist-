a0320efd243e7ac2c96393d19e0407aa05a974a3
Kenji.Yagi <kenji.yagi.jp@gmail.com>
2019-07-31 12:29:55 +0000

    課題修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_022332) do
+ActiveRecord::Schema.define(version: 2019_07_31_025251) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_07_30_022332) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
